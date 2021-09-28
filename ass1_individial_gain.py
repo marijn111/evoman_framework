4cfdaec1f21daafa80c681cd8499ee978d0f5757
marijn111 <39378193+marijn111@users.noreply.github.com>
2021-09-28 18:33:23 +0200

    update

--- ass1_individial_gain.py
+++ ass1_individial_gain.py
@@ -3,12 +3,14 @@ import pickle
 import sys
 sys.path.insert(0, 'evoman')
 from environment import Environment
+from ea2_environment import CustomEnvironment
 from ass1_controller import PlayerController
-from demo_controller import player_controller
 import matplotlib.pyplot as plt
 import neat
 import pandas as pd
 import seaborn as sns
+
+sns.set(font_scale=1.2)
 
 enemies = [1, 2, 3]
 neat_config_filename = 'neat_config.txt'
@@ -30,86 +32,55 @@ if __name__ == "__main__":
     if not os.path.exists(experiment_name):
         os.makedirs(experiment_name)
 
-    df = pd.DataFrame(columns=['enemy', 'EA', 'gain'])
-
-    determine_winners = True
-    winners_neat = [(0, -990), (0, -999), (0, -999)]
-    # winners_neuro = [(0, -999), (0, -999)]
+    df = pd.DataFrame(columns=['enemy', 'algo', 'gain'])
 
     for enemy in enemies:
-        if determine_winners:
-            for i in range(num_experiments):
-                # genome_neuro = pickle.load(open('winner_objects/neat-winner_{}_{}'.format(enemy, i), 'rb'))
-                genome_neat = pickle.load(open('winner_objects/neat-winner_{}_{}.pickle'.format(enemy, i), 'rb'))
-                ffn = neat.nn.FeedForwardNetwork.create(genome_neat, config)
-
-                total_gain_neat = 0
-                # total_gain_neuro = 0
-                for e in enemies:
-                    env_neat = Environment(
-                        experiment_name=experiment_name,
-                        enemies=[e],
-                        player_controller=PlayerController()
-                    )
-
-                    _, pe_neat, ee_neat, _ = env_neat.play(pcont=ffn)
-                    total_gain_neat += pe_neat - ee_neat
-
-                    # env_neuro = Environment(experiment_name=experiment_name,
-                    #                         enemies=[e],
-                    #                         player_controller=player_controller(10))
-                    #
-                    # _, pe_neuro, ee_neuro, _ = env_neuro.play(pcont=genome_neuro)
-                    # total_gain_neuro += pe_neuro - ee_neuro
-
-                if winners_neat[enemy-1][1] < total_gain_neat:
-                    winners_neat[enemy-1] = (i, total_gain_neat)
-                # if winners_neuro[enemy][1] < total_gain_neuro:
-                #     winners_neuro[enemy] = (i, total_gain_neuro)
-
-        print(winners_neat)
-        # print(winners_neuro)
-
-        # genome_neuro = pickle.load(open('winner_objects/neat-winner_{}_{}'.format(enemy, winners_neuro[enemy][0]), 'rb'))
-        genome_neat = pickle.load(open('winner_objects/neat-winner_{}_{}.pickle'.format(enemy, winners_neat[enemy-1][0]), 'rb'))
-        ffn = neat.nn.FeedForwardNetwork.create(genome_neat, config)
-
-        for v in range(num_best_solution_runs):
+        for i in range(num_experiments):
             total_gain_neat = 0
-            # total_gain_neuro = 0
-            for enem in enemies:
+            genome_neat = pickle.load(open('winner_objects/neat-winner_{}_{}.pickle'.format(enemy, i), 'rb'))
+            ffn = neat.nn.FeedForwardNetwork.create(genome_neat, config)
+            for j in range(num_best_solution_runs):
                 env_neat = Environment(experiment_name=experiment_name,
-                                       enemies=[enem],
+                                       enemies=[enemy],
                                        player_controller=PlayerController())
 
-                _, pe_neat, ee_neat, _ = env_neat.play(pcont=ffn)
-                total_gain_neat += pe_neat - ee_neat
+                _, player_life, enemy_life, _ = env_neat.play(pcont=ffn)
+                total_gain_neat += player_life - enemy_life
 
-                # env_neuro = Environment(experiment_name=experiment_name,
-                #                         enemies=[enem],
-                #                         player_controller=player_controller(10))
-                #
-                # _, pe_neuro, ee_neuro, _ = env_neuro.play(pcont=genome_neuro)
-                # total_gain_neuro += pe_neuro - ee_neuro
-
+            mean_gain = total_gain_neat / num_best_solution_runs
             df = df.append([{
                 'enemy': enemy,
-                'EA': 'neat',
-                'gain': total_gain_neat
+                'algo': 'neat',
+                'gain': mean_gain
             }
-                # , {
-            #     'enemy': enemy,
-            #     'EA': 'neuro',
-            #     'gain': total_gain_neuro
-            # }
             ], ignore_index=True)
 
-    print(winners_neat)
-    # print(winners_neuro)
-    df.to_csv('results-winner-gains.csv', index=False)
+    for enemy in enemies:
+        for i in range(num_experiments):
+            total_gain_custom_neat = 0
+            genome_neat = pickle.load(open('custom_winner_objects/neat-winner_{}_{}.pickle'.format(enemy, i), 'rb'))
+            ffn = neat.nn.FeedForwardNetwork.create(genome_neat, config)
+            for j in range(num_best_solution_runs):
+                env_neat = CustomEnvironment(
+                    experiment_name=experiment_name,
+                    enemies=[enemy],
+                    player_controller=PlayerController())
 
-    sns.boxplot(data=df, x='enemy', y='gain', hue='EA').set_title('gain of best solutions')
+                _, player_life, enemy_life, _ = env_neat.play(pcont=ffn)
+                total_gain_custom_neat += player_life - enemy_life
+
+            mean_gain = total_gain_custom_neat / num_best_solution_runs
+            df = df.append([{
+                'enemy': enemy,
+                'algo': 'custom_neat',
+                'gain': mean_gain
+            }
+            ], ignore_index=True)
+
+    df.to_csv('gains_winners_duo.csv', index=False)
+
+    sns.boxplot(data=df, x='enemy', y='gain', hue='algo').set_title('Individual Gain of Winner Genomes')
     sns.despine(offset=10, trim=True)
 
     plt.tight_layout()
-    plt.savefig('gains.png')
+    plt.savefig('gains_winners_duo.png')
--- ass1_visualization.py
+++ ass1_visualization.py
@@ -1,23 +1,26 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import matplotlib as mpl
 
-
-# sns.set(font_scale=1.2)
-# mpl.rcParams['figure.dpi'] = 300
+sns.set(font_scale=1.2)
+mpl.rcParams['figure.dpi'] = 300
 
 enemies = [1, 2, 3]
 
 df = pd.read_csv('neat.csv')
+df2 = pd.read_csv('custom_neat.csv')
 
 for e in enemies:
-    e_df = df.loc[df['enemy'] == e]
+    # e_df = df.loc[df['enemy'] == e]
+    e_df = pd.concat([df.loc[df['enemy'] == e], df2.loc[df['enemy'] == e]])
 
     ax = sns.lineplot(data=e_df, x='gen', y='fitness', hue='algo', style='type')
     ax.set(xlabel='generation', ylabel='fitness', title=f'enemy {e}')
 
     plt.tight_layout()
-    plt.savefig(f'ea1-lineplot-enemy-{e}.png')
+    # plt.savefig(f'ea1-lineplot-enemy-{e}.png')
+    plt.savefig(f'duo-lineplot-enemy-{e}.png')
     plt.clf()
     plt.cla()
 
