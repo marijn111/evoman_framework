1d2794cf1696cefbf961b6422fe6a23ed1d25419
marijn111 <39378193+marijn111@users.noreply.github.com>
2021-10-13 15:20:36 +0200

    update

--- ass2_individual_gain.py
+++ ass2_individual_gain.py
@@ -16,6 +16,7 @@ enemy_groups = [[1, 2, 3], [3, 4, 5]]
 neat_config_filename = 'neat_config.txt'
 num_experiments = 10
 num_best_solution_runs = 5
+num_enemies = 8
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 
@@ -32,29 +33,33 @@ if __name__ == "__main__":
     if not os.path.exists(experiment_name):
         os.makedirs(experiment_name)
 
-    df = pd.DataFrame(columns=['enemy', 'algo', 'gain'])
+    df = pd.DataFrame(columns=['group', 'algo', 'gain'])
 
     for e, enemy_group in enumerate(enemy_groups):
         for i in range(num_experiments):
             total_gain_neat = 0
-            genome_neat = pickle.load(open('winner_objects/neat-winner_{}_{}.pickle'.format(e, i), 'rb'))
+            genome_neat = pickle.load(open('neat_winners/generalist-winner_{}_{}.pickle'.format(e, i), 'rb'))
             ffn = neat.nn.FeedForwardNetwork.create(genome_neat, config)
 
-            for j in range(num_best_solution_runs):
-                env_neat = Environment(experiment_name=experiment_name,
-                                       enemies=enemy_group,
-                                       player_controller=PlayerController())
+            for enem in range(1, num_enemies+1):
+                mean_total_gain = 0
+                for j in range(num_best_solution_runs):
+                    env_neat = Environment(experiment_name=experiment_name,
+                                           enemies=[enem],
+                                           player_controller=PlayerController())
 
-                _, player_life, enemy_life, _ = env_neat.play(pcont=ffn)
-                total_gain_neat += player_life - enemy_life
+                    _, player_life, enemy_life, _ = env_neat.play(pcont=ffn)
+                    mean_total_gain += player_life - enemy_life
 
-            mean_gain = total_gain_neat / num_best_solution_runs
+                total_gain_neat += mean_total_gain / num_best_solution_runs
+            mean_gain = total_gain_neat / num_enemies
+
             df = df.append([{
-                'group': e,
-                'algo': 'neat',
-                'gain': mean_gain
-            }
-            ], ignore_index=True)
+                    'group': e,
+                    'algo': 'neat',
+                    'gain': mean_gain
+                }
+                ], ignore_index=True)
 
     for e, enemy_group in enumerate(enemy_groups):
         for i in range(num_experiments):
@@ -65,15 +70,15 @@ if __name__ == "__main__":
                 mean_total_gain = 0
                 for j in range(num_best_solution_runs):
                     env_neuro = Environment(
-                        experiment_name=experiment_name,
-                        enemies=[enem],
-                        player_controller=player_controller(10))
+                            experiment_name=experiment_name,
+                            enemies=[enem],
+                            player_controller=player_controller(10))
 
                     _, player_life, enemy_life, _ = env_neuro.play(pcont=genome_neuro)
                     mean_total_gain += player_life - enemy_life
 
                 total_gain_neuro += mean_total_gain / num_best_solution_runs
-            mean_gain = total_gain_neuro / num_best_solution_runs
+            mean_gain = total_gain_neuro / num_enemies
 
             df = df.append([{
                 'group': e,
