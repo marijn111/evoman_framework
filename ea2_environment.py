2acca1cc526fb30ac68694a493fee16f3984bdbe
marijn111 <39378193+marijn111@users.noreply.github.com>
2021-10-18 18:39:49 +0200

    cleanup

--- ea2_environment.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, 'evoman')
-from environment import Environment
-import numpy as np
-
-# fitness function = (100-ee)*1-(100-ep)*2-(log t)*2
-class CustomEnvironment(Environment):
-    def fitness_single(self):
-        return (100-self.get_enemylife())*1-(100-self.get_playerlife())*2-(np.log(self.get_time()))*2
